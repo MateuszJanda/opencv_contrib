661cc647593c566f34951279d6a5d0b075bb5f51
Mateusz Janda <mateusz.janda@gmail.com>
2020-03-06 22:53:11 +0100

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 
-g++ -I../output/include/opencv4 -L../output/lib -Wl,-rpath=../output/lib main.cpp -lopencv_core -lopencv_imgcodecs -lopencv_imgproc -lopencv_shape
+g++ -I../output/include/opencv4 -L../output/lib -Wl,-rpath=../output/lib main.cpp -lopencv_core -lopencv_imgcodecs -lopencv_imgproc -lopencv_shape;
+rm core;
+ulimit -c unlimited;
+./a.out;
