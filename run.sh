622554e2612eb7853c24f3205c73a4d3647ec2b6
Mateusz Janda <mateusz.janda@gmail.com>
2020-03-10 21:51:21 +0100

    Update paths

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-g++ -I../output_bug/include/opencv4 -L../output_bug/lib -Wl,-rpath=../output_bug/lib main.cpp -lopencv_core -lopencv_imgcodecs -lopencv_imgproc -lopencv_shape;
+g++ -I../venv/local/include/opencv4 -L../venv/local/lib -Wl,-rpath=../venv/local/lib main.cpp -lopencv_core -lopencv_imgcodecs -lopencv_imgproc -lopencv_shape;
 rm core;
 ulimit -c unlimited;
 ./a.out;
